023598975ee0efea58fb5df51ca5e57d08a4b178
dima1997 <franki.dimaria@gmail.com>
2025-01-17 17:30:07 -0300

    refactor: PlayerCreate only takes user_public_id and telegram_id

--- app/models/player.py
+++ app/models/player.py
@@ -3,20 +3,19 @@ import uuid
 from sqlmodel import Field, Index, SQLModel
 
 
-# Shared properties
-class PlayerBase(SQLModel):
+# Properties to receive on player creation
+class PlayerCreate(SQLModel):
     user_public_id: uuid.UUID = Field(unique=True)
     telegram_id: int | None = Field(default=None)
+
+
+# Shared properties
+class PlayerBase(PlayerCreate):
     zone_km: int | None = Field(default=None)
     zone_location: str | None = Field(default=None)
     latitude: float | None = Field(default=None)
     longitude: float | None = Field(default=None)
     time_availability: int | None = Field(default=None)
-
-
-# Properties to receive on player creation
-class PlayerCreate(PlayerBase):
-    pass
 
 
 # Database model, database table inferred from class name
