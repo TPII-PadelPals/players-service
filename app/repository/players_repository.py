f8f2f7da729ee5376638f96d6461db22ee942254
dima1997 <franki.dimaria@gmail.com>
2025-01-17 17:47:33 -0300

    feat: create player with  user_public_id that already exists responds 409

--- app/repository/players_repository.py
+++ app/repository/players_repository.py
@@ -1,23 +1,27 @@
+from typing import Any
+
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.models.player import Player, PlayerCreate
+from app.utilities.exceptions import NotUniqueException
 
 
 class PlayersRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    # async def _check_unique_attr(self, attr: Any, value: Any) -> None:
-    #     player_attr = getattr(Player, attr)
-    #     statement = select(player_attr).where(player_attr == value)
-    #     attr_exist = (await self.session.exec(statement)).first()
-    #     if attr_exist:
-    #         raise NotUniqueException(item=attr.capitalize())
+    async def _check_unique_attr(self, attr: Any, value: Any) -> None:
+        player_attr = getattr(Player, attr)
+        statement = select(player_attr).where(player_attr == value)
+        attr_exist = (await self.session.exec(statement)).first()
+        if attr_exist:
+            attr_str = " ".join(attr.split("_"))
+            raise NotUniqueException(item=attr_str.capitalize())
 
     async def create_player(self, player_in: PlayerCreate) -> Player:
         player = Player.model_validate(player_in)
-        # await self._check_unique_attr("email", player.email)
-        # await self._check_unique_attr("phone", player.phone)
+        await self._check_unique_attr("user_public_id", player.user_public_id)
         self.session.add(player)
         await self.session.commit()
         await self.session.refresh(player)
--- app/tests/api/routes/test_players.py
+++ app/tests/api/routes/test_players.py
@@ -26,3 +26,22 @@ async def test_create_user(
     assert content["latitude"] is None
     assert content["longitude"] is None
     assert content["time_availability"] is None
+
+
+async def test_create_player_user_public_id_already_exists_responds_409(
+    async_client: AsyncClient, x_api_key_header: dict[str, str]
+) -> None:
+    user_public_id = str(uuid.uuid4())
+
+    data = {"user_public_id": user_public_id, "telegram_id": 11112222}
+    await async_client.post(
+        f"{settings.API_V1_STR}/players/", headers=x_api_key_header, json=data
+    )
+
+    data = {"user_public_id": user_public_id, "telegram_id": 33334444}
+    response = await async_client.post(
+        f"{settings.API_V1_STR}/players/", headers=x_api_key_header, json=data
+    )
+    assert response.status_code == 409
+    content = response.json()
+    assert content["detail"] == "User public id already exists."
