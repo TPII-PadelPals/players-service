820f784d9fffb39137a57baf7a45247c210c5637
dima1997 <franki.dimaria@gmail.com>
2025-01-20 16:06:55 -0300

    refactor: return player in one line; add user_public_id arg type

--- app/services/players_service.py
+++ app/services/players_service.py
@@ -1,3 +1,5 @@
+import uuid
+
 from app.models.player import Player, PlayerCreate, PlayerUpdate
 from app.repository.players_repository import PlayersRepository
 from app.utilities.dependencies import SessionDep
@@ -12,8 +14,7 @@ class PlayersService:
         return player
 
     async def update_player(
-        self, session: SessionDep, user_public_id, player_in: PlayerUpdate
+        self, session: SessionDep, user_public_id: uuid.UUID, player_in: PlayerUpdate
     ) -> Player:
         repo = PlayersRepository(session)
-        player = await repo.update_player(user_public_id, player_in)
-        return player
+        return await repo.update_player(user_public_id, player_in)
