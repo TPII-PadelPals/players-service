93a142770542c82c8b07b7c51409ddff058cd733
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-01-31 16:28:59 -0300

    test: add test update stroke

--- app/tests/api/routes/test_padel_strokes.py
+++ app/tests/api/routes/test_padel_strokes.py
@@ -4,6 +4,7 @@ from httpx import AsyncClient
 
 from app.core.config import settings
 from app.models.padel_stroke import DEFINITION_OF_CATEGORIZATION
+from app.services.padel_strokes_service import PadelStrokesService
 
 
 async def test_create_player_create_stroke_associated(
@@ -28,7 +29,6 @@ async def test_create_player_create_stroke_associated(
     # assert
     assert response_stroke.status_code == 200
     content = response_stroke.json()
-    print("ASDASDASD: ", content, type(content))
     assert content["user_public_id"] == user_public_id_str
     for field, value in content.items():
         if field == "user_public_id":
@@ -36,3 +36,44 @@ async def test_create_player_create_stroke_associated(
         else:
             assert value == DEFINITION_OF_CATEGORIZATION[0]
 
+async def test_update_stroke(
+        async_client: AsyncClient, x_api_key_header: dict[str, str]
+) -> None:
+    user_public_id = uuid.uuid4()
+    user_public_id_str = str(user_public_id)
+    telegram_id = 10103030
+
+    data_player = {"user_public_id": user_public_id_str, "telegram_id": telegram_id}
+
+    response_player = await async_client.post(
+        f"{settings.API_V1_STR}/players/", headers=x_api_key_header, json=data_player
+    )
+    # If the test fails it has no meaning
+    assert response_player.status_code == 201
+
+    change_intermediate = ("background_ground", "forehand_back_wall", "backhand_back_wall")
+    change_advance = ("forehand_volley", "backhand_volley")
+    data = {
+        "background_ground": DEFINITION_OF_CATEGORIZATION[1],
+        "forehand_back_wall": DEFINITION_OF_CATEGORIZATION[1],
+        "backhand_back_wall": DEFINITION_OF_CATEGORIZATION[1],
+        "forehand_volley": DEFINITION_OF_CATEGORIZATION[2],
+        "backhand_volley": DEFINITION_OF_CATEGORIZATION[2],
+    }
+    # test
+    response = await async_client.put(
+        f"{settings.API_V1_STR}/padel-strokes/", headers=x_api_key_header, params={"user_public_id": user_public_id_str}, json=data
+    )
+    # assert
+    assert response.status_code == 200
+    content = response.json()
+    assert content["user_public_id"] == user_public_id_str
+    for field, value in content.items():
+        if field == "user_public_id":
+            assert value == user_public_id_str
+        elif field in change_intermediate:
+            assert value == DEFINITION_OF_CATEGORIZATION[1]
+        elif field in change_advance:
+            assert value == DEFINITION_OF_CATEGORIZATION[2]
+        else:
+            assert value == DEFINITION_OF_CATEGORIZATION[0]
