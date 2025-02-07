8723b875935c7114cdecb706c8854a2362967791
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-02-07 15:54:56 -0300

    fix: order of creation of player and strokes

--- app/services/players_and_strokes_service.py
+++ app/services/players_and_strokes_service.py
@@ -10,22 +10,20 @@ class PlayersAndStrokesService:
     async def create_player(
         self, session: SessionDep, player_in: PlayerCreate
     ) -> Player:
+        player = await self._create_only_player(session, player_in)
         stroke = await self._create_strokes(session, player_in)
         # All data that needs to be refreshed must be in the list
-        other_for_refresh = [stroke]
-        # This is the final function (commit and refresh are done)
-        player = await self._create_only_player_and_finish(session, player_in, other_for_refresh)
+        other_for_refresh = [player, stroke]
+        # This is the final function (refresh are done)
+        await self._finish_transaction(session, other_for_refresh)
         return player
 
 
-    async def _create_only_player_and_finish(self, session: SessionDep, player_in: PlayerCreate, other_for_refresh: list) -> Player:
+    async def _create_only_player(self, session: SessionDep, player_in: PlayerCreate) -> Player:
         service_player = PlayersService()
         try:
             player = await service_player.create_player(session, player_in)
-            await session.commit()
-            await session.refresh(player)
-            for item in other_for_refresh:
-                await session.refresh(item)
+            # The commit is not made because it is not the final function
             return player
         except IntegrityError:
             await self._raise_not_unique(session)
@@ -45,6 +43,18 @@ class PlayersAndStrokesService:
         except IntegrityError:
             await self._raise_not_unique(session)
         except Exception as e:
+            raise e
+
+
+    async def _finish_transaction(self, session: SessionDep, other_for_refresh: list):
+        try:
+            await session.commit()
+            for item in other_for_refresh:
+                await session.refresh(item)
+        except IntegrityError:
+            await self._raise_not_unique(session)
+        except Exception as e:
+            await session.rollback()
             raise e
 
 
