4ed6db51764faf07a48762cba36d51a379161f2b
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-02-11 16:57:36 -0300

    fix: fix

--- app/tests/repository/test_strokes_repository.py
+++ app/tests/repository/test_strokes_repository.py
@@ -3,19 +3,34 @@ import uuid
 import pytest
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from app.models.player import Player, PlayerCreate
 from app.models.strokes import StrokeCreate, StrokeUpdate
+from app.repository.players_repository import PlayersRepository
 from app.repository.strokes_repository import StrokesRepository
 from app.services.strokes_service import StrokesService
 from app.utilities.exceptions import NotFoundException
+
+
+async def create_player(session: AsyncSession, user_public_id: uuid.UUID) -> Player:
+    user_public_id_str = str(user_public_id)
+    telegram_id = 10103030
+    repo = PlayersRepository(session)
+    player_create = PlayerCreate(
+        user_public_id=user_public_id_str, telegram_id=telegram_id
+    )
+    player = await repo.create_player(player_create)
+    return player
 
 
 async def test_create_strokes(session: AsyncSession) -> None:
     user_public_id = uuid.uuid4()
     repo = StrokesRepository(session)
     info_create = StrokeCreate(background_ground=2.0, backhand_volley=3.0)
+    player = await create_player(session, user_public_id)
     # test
     result = await repo.create_strokes(info_create, user_public_id)
     await session.commit()
+    await session.refresh(player)
     await session.refresh(result)
     # assert
     for field in result.__dict__:
@@ -37,8 +52,10 @@ async def test_get_strokes(session: AsyncSession) -> None:
     service = StrokesService()
     repo = StrokesRepository(session)
     info_create = StrokeCreate(background_ground=2.0, backhand_volley=3.0)
+    player = await create_player(session, user_public_id)
     stroke = await service.create_strokes(session, info_create, user_public_id)
     await session.commit()
+    await session.refresh(player)
     await session.refresh(stroke)
     # test
     result = await repo.get_strokes(user_public_id)
@@ -74,8 +91,10 @@ async def test_update_padel_strokes(session: AsyncSession) -> None:
     service = StrokesService()
     repo = StrokesRepository(session)
     info_create = StrokeCreate(background_ground=2.0, backhand_volley=3.0)
+    player = await create_player(session, user_public_id)
     stroke = await service.create_strokes(session, info_create, user_public_id)
     await session.commit()
+    await session.refresh(player)
     await session.refresh(stroke)
     update_info = StrokeUpdate(background_ground=3.0, forehand_ground=2.0)
     # test
--- app/tests/services/test_strokes_service.py
+++ app/tests/services/test_strokes_service.py
@@ -2,9 +2,21 @@ import uuid
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from app.models.player import PlayerCreate
+from app.models.player import Player, PlayerCreate
 from app.services.players_and_strokes_service import PlayersAndStrokesService
+from app.services.players_service import PlayersService
 from app.services.strokes_service import StrokesService
+
+
+async def create_player(session: AsyncSession, user_public_id: uuid.UUID) -> Player:
+    user_public_id_str = str(user_public_id)
+    telegram_id = 10103030
+    player_service = PlayersService()
+    player_create = PlayerCreate(
+        user_public_id=user_public_id_str, telegram_id=telegram_id
+    )
+    player = await player_service.create_player(session, player_create)
+    return player
 
 
 async def test_create_strokes_defaults_to_beginner_base_level(
@@ -12,10 +24,12 @@ async def test_create_strokes_defaults_to_beginner_base_level(
 ) -> None:
     user_public_id = uuid.uuid4()
     service = StrokesService()
+    player = await create_player(session, user_public_id)
     # test
     info_strokes = None
     stroke = await service.create_strokes(session, info_strokes, user_public_id)
     await session.commit()
+    await session.refresh(player)
     await session.refresh(stroke)
     # assert
     for field in stroke.__dict__:
@@ -32,8 +46,10 @@ async def test_create_and_get_strokes(session: AsyncSession) -> None:
     user_public_id = uuid.uuid4()
     service = StrokesService()
     info_strokes = None
+    player = await create_player(session, user_public_id)
     stroke_created = await service.create_strokes(session, info_strokes, user_public_id)
     await session.commit()
+    await session.refresh(player)
     await session.refresh(stroke_created)
     # test
     stroke = await service.get_strokes(session, user_public_id)
